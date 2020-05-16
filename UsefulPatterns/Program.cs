cd57b2bfd6dedd241a37e3f4a523685af15fe47d
Christian Aranda Diaz <c.aranda.diaz@avanade.com>
2020-05-16 11:13:56 +0200

    - Delete unnused

--- UsefulPatterns/Program.cs
+++ UsefulPatterns/Program.cs
@@ -1,26 +1,10 @@
-﻿using System;
-using UsefulPatterns.Builder;
-
-namespace UsefulPatterns
+﻿namespace UsefulPatterns
 {
     internal class Program
     {
         private static void Main()
         {
-            //var decorator = new DecoratorStarter();
-            //decorator.Run();
 
-            //Console.ReadKey();
-
-            ICarBuilder carBuilder = new CityCarBuilder();
-            
-            var car = carBuilder
-                .AddExtraPower()
-                .AddExtraPower()
-                .ChangeEngine()
-                .Build();
-
-            Console.WriteLine($"== Build car == {Environment.NewLine}{car.Debug()}");
         }
     }
 }
